21c08d94b1d0ebcccc74b43f8247bc5dd19a4743
WeetHet <stas.ale66@gmail.com>
2025-11-17 20:12:51 +0200

    build: switch to nix, uv, yarn

---
+++ shell.nix
@@ -0,0 +1,69 @@
+{
+  system ? builtins.currentSystem,
+  sources ? import ./npins,
+  pkgs ? import sources.nixpkgs { inherit system; },
+  lib ? pkgs.lib,
+  pyproject-nix ? import sources.pyproject-nix { inherit lib; },
+  uv2nix ? import sources.uv2nix { inherit lib pyproject-nix; },
+  pyproject-build-systems ? import sources.build-system-pkgs {
+    inherit uv2nix pyproject-nix lib;
+  },
+}:
+let
+  workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+  overlay = workspace.mkPyprojectOverlay {
+    sourcePreference = "wheel";
+  };
+  editableOverlay = workspace.mkEditablePyprojectOverlay {
+    root = "$REPO_ROOT";
+  };
+  overrides-sdist = import ./nix/overrides-sdist.nix {
+    src = lib.fileset.toSource rec {
+      root = ./.;
+      fileset = lib.fileset.unions (
+        map (path: root + path) [
+          "/pyproject.toml"
+          "/lms/"
+          "/conftest.py"
+          "/manage.py"
+          "/pytest.ini"
+          "/README.md"
+        ]
+      );
+    };
+  };
+  overlays = lib.composeManyExtensions [
+    pyproject-build-systems.wheel
+    overlay
+    overrides-sdist
+  ];
+  python = pkgs.python310;
+  pythonBase =
+    (pkgs.callPackage pyproject-nix.build.packages {
+      inherit python;
+    }).overrideScope
+      overlays;
+  virtualenv = (pythonBase.overrideScope editableOverlay).mkVirtualEnv "lms-env" workspace.deps.all;
+in
+pkgs.mkShell {
+  packages = [
+    virtualenv
+    pkgs.uv
+
+    pkgs.nodejs_20
+    pkgs.yarn-berry
+
+    pkgs.ruff
+    pkgs.nixfmt-tree
+  ];
+
+  env = {
+    UV_NO_SYNC = "1";
+    UV_PYTHON = pythonBase.python.interpreter;
+    UV_PYTHON_DOWNLOADS = "never";
+  };
+  shellHook = ''
+    unset PYTHONPATH
+    export REPO_ROOT=$(git rev-parse --show-toplevel)
+  '';
+}
