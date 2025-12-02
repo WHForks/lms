3c94b575abb29876c109d86497dfb1e368895b7f
WeetHet <stas.ale66@gmail.com>
2025-12-02 17:13:38 +0200

    build(nix): add flake.nix for garnix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  outputs =
+    { ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSystem =
+        f:
+        builtins.listToAttrs (
+          builtins.map (system: {
+            name = system;
+            value = f system;
+          }) systems
+        );
+    in
+    {
+      packages = forEachSystem (system: (import ./default.nix { inherit system; }).packages);
+    };
+}
